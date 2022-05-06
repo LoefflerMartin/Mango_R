8c40b97dd462e515e31eb2b1ba49b723ea08104f
LoefflerMartin <martin_loeffler@gmx.net>
2022-05-06 16:45:39 +0200

    upload to repo

--- version_control_collection.R
+++ version_control_collection.R
@@ -4,6 +4,6 @@ library(usethis)
 use_git_config(user.name = "LoefflerMartin", user.email = "martin_loeffler@gmx.net")
 
 usethis::create_github_token()
-"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"
+
 gitcreds::gitcreds_set()
 
