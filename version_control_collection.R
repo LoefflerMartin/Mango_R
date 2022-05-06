51fbf96f906047ec46e59b909548f9fd283e1edd
LoefflerMartin <martin_loeffler@gmx.net>
2022-05-06 16:55:55 +0200

    delete version_control

--- version_control_collection.R
+++
@@ -1,9 +0,0 @@
-install.packages("renv")
-install.packages("usethis")
-library(usethis)
-use_git_config(user.name = "LoefflerMartin", user.email = "martin_loeffler@gmx.net")
-
-usethis::create_github_token()
-
-gitcreds::gitcreds_set()
-
